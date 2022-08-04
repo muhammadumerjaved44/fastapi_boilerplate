84347755dceb19965fd6c43409f1efd29ab75303
Muhammad Umer Javed <muhammadumerjaved44@gmail.com>
2022-08-04 13:21:16 +0500

    improved code by using FastAPI statuses

--- api/routes/utils.py
+++ api/routes/utils.py
@@ -1,5 +1,5 @@
 from email_validator import validate_email, EmailNotValidError
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, status
 from schemas import RequestDemoIn, RequestDemoOut
 import models
 import auth
@@ -15,21 +15,15 @@ async def ping():
     return response
 
 
-@router.get("/hash_password")
-async def hash_password(password: str):
-
-    hashed_password = auth.get_password_hash(password)
-    response: dict = {"hashed_password": hashed_password}
-    return response
-
-
 @router.post("/request_demo", response_model=RequestDemoOut)
 async def request_demo(user_details: RequestDemoIn):
 
     try:
         validate_email(user_details.email).email
     except EmailNotValidError:
-        raise HTTPException(status_code=400, detail="Email not valid")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Email not valid"
+        )
 
     response: RequestDemoOut = RequestDemoOut(
         message="Demo request successfully accepted"
