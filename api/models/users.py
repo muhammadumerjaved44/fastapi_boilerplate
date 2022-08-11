2ad6472c1e6aa7138c1653d175512e7bd30b8556
Muhammad Umer Javed <muhammadumerjaved44@gmail.com>
2022-08-11 16:31:58 +0500

    User upgrade

--- api/models/users.py
+++ api/models/users.py
@@ -12,6 +12,6 @@ class User(Base):
     password = Column(String(255), nullable=False)
     first_name = Column(String(50))
     last_name = Column(String(50))
-    is_admin = Column(Boolean, default=False)
     is_active = Column(Boolean, default=False)
+    scope = Column(String(10))
     contacts = relationship("Contact")
